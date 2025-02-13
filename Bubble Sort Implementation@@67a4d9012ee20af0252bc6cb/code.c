25519ea82cc88837314684eb80b148dab4f9dabc
Sonam_Negi <sonamnorphelnegi06@gmail.com>
2025-02-13 12:57:01 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
-void bubble(int arr[],int n){
-    for (int i =0;i<n-1;i++){
-        for ( int j =0;j<n-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                int temp =arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
-void Array(int arr[],int n){
-    for (int i=0;i<n;i++){
-        printf("%d",arr[i]);
-        if(i<n-1){
-            printf(" ");}
 
+void printArray(int arr[], int n) {  
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
     }
     printf("\n");
 }
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for (int i =0;i<n;i++){
-        scanf("%d",&arr[i]);
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    bubbleSort(arr,n);
-    PrintArray(arr,n);
+    
+    bubbleSort(arr, n);
+    printArray(arr, n);  
+
     return 0;
 }
